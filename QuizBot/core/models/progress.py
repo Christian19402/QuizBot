3ea92a09fd51152ab94dbff62f598b36fa080314
Christian <166588588+Christian19402@users.noreply.github.com>
2025-03-02 19:42:01 -0500

    Add files via upload

---
+++ QuizBot/core/models/progress.py
@@ -0,0 +1,15 @@
+class Progress:
+    def __init__(self, user_id, subject_id, topic, correct_answers, total_questions):
+        self.user_id = user_id
+        self.subject_id = subject_id
+        self.topic = topic
+        self.correct_answers = correct_answers
+        self.total_questions = total_questions
+
+
+
+
+
+
+
+        
